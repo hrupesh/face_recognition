8e4fc91df27c59b37dd834b036d685044de04220
Rupesh <rcashok0@gmail.com>
2019-07-22 03:09:19 +0530

    Trying again

--- frontend/api.py
+++ frontend/api.py
@@ -1,8 +1,8 @@
-from .models import User
-from rest_framework import viewsets , permissions
-from .serializers import UserSerializer
+#from .models import User
+#from rest_framework import viewsets , permissions
+#from .serializers import UserSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
+#class UserViewSet(viewsets.ModelViewSet):
     
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+ #   serializer_class = UserSerializer
+  #  queryset = User.objects.all()
